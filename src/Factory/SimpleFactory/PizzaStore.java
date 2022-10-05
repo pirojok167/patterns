efee40178c2d6d10cc9d4b8c4d5fbcd307af3c97
pirojok167@gmail.com <PrOfIlE167%>
2022-10-05 21:18:39 +0400

    Refactored simple factory

--- src/Factory/SimpleFactory/PizzaStore.java
+++ src/Factory/SimpleFactory/PizzaStore.java
@@ -7,6 +7,16 @@ public class PizzaStore {
         this.pizzaFactory = pizzaFactory;
     }
 
+    public static void main(String[] args) {
+        PizzaFactory pizzaFactory = new PizzaFactory();
+
+        PizzaStore pizzaStore = new PizzaStore(pizzaFactory);
+
+        Pizza pizza = pizzaStore.makePizza("cheese");
+
+        System.out.println(pizza);
+    }
+
     public Pizza makePizza(String pizzaType) {
         Pizza pizza = pizzaFactory.createPizza(pizzaType);
 
